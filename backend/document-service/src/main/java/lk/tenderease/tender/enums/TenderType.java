e5e5e57eab349826fc0974e8cf7a6c66f3d19419
Nisith Moragoda <88430845+n1s1th@users.noreply.github.com>
2026-06-29 03:28:48 +0530

    fix : Tender Issues

--- backend/document-service/src/main/java/lk/tenderease/tender/enums/TenderType.java
+++ backend/document-service/src/main/java/lk/tenderease/tender/enums/TenderType.java
@@ -1,5 +1,14 @@
 package lk.tenderease.tender.enums;
 
 public enum TenderType {
-    OPEN_TENDER, RESTRICTED, FRAMEWORK_AGREEMENT
+    OPEN_TENDER,
+    RESTRICTED,
+    FRAMEWORK_AGREEMENT,
+    INTERNATIONAL,
+    NATIONAL,
+    SELECTIVE,
+    TWO_STAGE,
+    DESIGN_BUILD,
+    TURNKEY,
+    EMERGENCY
 }
