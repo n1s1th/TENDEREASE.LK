46926fcfb1ad21922c5f8f71599a0c89b24df182
Sirisooriya S.T.N <sirisooriyastn.23@uom.lk>
2026-08-24 02:22:08 +0530

    fix(frontend): actually use correct env var for officer api

--- frontend/lib/api/officerApi.ts
+++ frontend/lib/api/officerApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { OfficerRegistrationFormData } from '../validations/officerSchema';
 
-const API_BASE_URL = '\/officers';
+const API_BASE_URL = process.env.NEXT_PUBLIC_USER_API_URL ? `${process.env.NEXT_PUBLIC_USER_API_URL}/officers` : 'http://localhost:8081/api/officers';
 
 export interface OfficerSuccessResponse {
   success: boolean;
